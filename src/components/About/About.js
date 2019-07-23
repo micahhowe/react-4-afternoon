84e9593244bb1df5b1d9f42ef60ae8c0e04e7dd6
micahhowe <micahhowe182@gmail.com>
2019-07-23 15:50:59 -0600

    added the sub nav menu on the about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 
 export default class About extends Component {
   render() {
     return (
       <div>
+        
         <div className='subnav'>
+          <Link className="subnav_links" to="/about">
+            <h3>About</h3>
+          </Link>
+          <Link className="subnav_links" to="/about/history">
+            <h3>History</h3>
+          </Link>
+          <Link className="subnav_links" to="/about/contact">
+            <h3>Contact</h3>
+          </Link>
         </div>
         <div className='box'>
         </div>
